b09afa5ae88d2f96ba80d14f448043f2a33cc3fe
Jon Lundy <jon.lundy@rakuten.com>
2018-03-27 14:07:06 -0600

    add padding handling

--- cmd/totp/main.go
+++ cmd/totp/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"github.com/JonLundy/go-totp/totp"
+	"github.com/JonLundy/go-totp/pkg/totp"
 	"bufio"
 	"os"
 	"encoding/base32"
 	"time"
 	"crypto/sha1"
 	"fmt"
+        "strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	key, _ := reader.ReadString('\n')
 
-	k, _ := base32.StdEncoding.DecodeString(key)
+	key = strings.Trim(key, "=\n")
+
+        if i := len(key) % 8; i != 0 {
+           key += strings.Repeat("=", 8-i)
+        }
+
+        k, err := base32.StdEncoding.DecodeString(key)
 
 	code, _ := totp.Totp(k, time.Now().Unix(), sha1.New, 6)
 	fmt.Print(code)
 }
+
